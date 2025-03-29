15d71078896194df877d6ea095f313d272148ddb
Free <jaime.vrivera@gmail.com>
2025-03-29 11:30:46 +0100

    insertar bien

--- auto-veloz/backend/insertarCoches.py
+++ auto-veloz/backend/insertarCoches.py
@@ -1,7 +1,8 @@
 import psycopg2
 
 class Coche:
-    def __init__(self, matricula, categoria, marca, modelo, plazas, techo, marcha, puertas, wifi, gps, silla_nino, cadenas):
+    def __init__(self, matricula, categoria, marca, modelo, plazas, techo, marcha, puertas, wifi, gps, silla_nino, cadenas, id_oficina_actual):
+        #self.id_vehiculo = id_vehiculo  # Generar un ID único para cada coche
         self.matricula = matricula
         self.categoria = categoria
         self.marca = marca
@@ -14,12 +15,13 @@ class Coche:
         self.gps = gps  # True o False
         self.silla_nino = silla_nino  # True o False
         self.cadenas = cadenas  # True o False
+        self.id_oficina_actual = id_oficina_actual
 
     def mostrar_info(self):
         return f"Matrícula: {self.matricula}, Categoría: {self.categoria}, Marca: {self.marca}, Modelo: {self.modelo}, " \
                f"Plazas: {self.plazas}, Techo: {'Sí' if self.techo else 'No'}, Marcha: {self.marcha}, Puertas: {self.puertas}, " \
                f"WiFi: {'Sí' if self.wifi else 'No'}, GPS: {'Sí' if self.gps else 'No'}, " \
-               f"Silla Niño: {'Sí' if self.silla_nino else 'No'}, Cadenas: {'Sí' if self.cadenas else 'No'}"
+               f"Silla Niño: {'Sí' if self.silla_nino else 'No'}, Cadenas: {'Sí' if self.cadenas else 'No'}, Oficina Actual: {self.id_oficina_actual}"
 
     def guardar_en_db(self):
         conexion = psycopg2.connect(
@@ -32,11 +34,11 @@ class Coche:
         cursor = conexion.cursor()
         cursor.execute(
             """
-            INSERT INTO coches (matricula, categoria, marca, modelo, plazas, techo, marcha, puertas, wifi, gps, silla_nino, cadenas)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO vehiculo (matricula,  marca, modelo, categoria, plazas, techo, tipo_marcha, num_puertas, wifi, gps, silla_nino, cadenas, id_oficina_actual)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             ON CONFLICT (matricula) DO NOTHING
             """,
-            (self.matricula, self.categoria, self.marca, self.modelo, self.plazas, self.techo, self.marcha, self.puertas, self.wifi, self.gps, self.silla_nino, self.cadenas)
+            (self.matricula, self.categoria, self.marca, self.modelo, self.plazas, self.techo, self.marcha, self.puertas, self.wifi, self.gps, self.silla_nino, self.cadenas, self.id_oficina_actual)
         )
         conexion.commit()
         cursor.close()
@@ -52,7 +54,7 @@ class Coche:
             port="5432"
         )
         cursor = conexion.cursor()
-        cursor.execute("SELECT * FROM coches WHERE matricula = %s", (matricula,))
+        cursor.execute("SELECT * FROM vehiculo WHERE matricula = %s", (matricula,))
         coche_data = cursor.fetchone()
         cursor.close()
         conexion.close()
@@ -64,32 +66,32 @@ class Coche:
 
 # Ejemplo de uso
 coches = [
-    Coche("1234ABC", "SUV", "Toyota", "RAV4", 5, True, "automático", 5, True, True, False, True),
-    Coche("4200EXX", "SUV", "Hyundai", "Tucson", 5, True, "manual", 5, True, True, False, True),
-    Coche("5678DEF", "Sedán", "Honda", "Civic", 5, False, "automático", 4, True, True, True, False),
-    Coche("9101GHI", "Hatchback", "Ford", "Focus", 5, False, "manual", 4, False, True, False, False),
-    Coche("1122JKL", "Coupé", "BMW", "Serie 4", 4, True, "automático", 2, True, True, False, False),
-    Coche("3344MNO", "SUV", "Mercedes", "GLA", 5, True, "automático", 5, True, True, True, True),
-    Coche("5566PQR", "Pickup", "Chevrolet", "Silverado", 5, False, "manual", 4, False, True, False, True),
-    Coche("7788STU", "Sedán", "Audi", "A4", 5, True, "automático", 4, True, True, False, False),
-    Coche("9900VWX", "Convertible", "Mazda", "MX-5", 2, True, "manual", 2, False, False, False, False),
-    Coche("1111YYY", "SUV", "Nissan", "X-Trail", 7, True, "automático", 5, True, True, True, True),
-    Coche("2222ZZZ", "Hatchback", "Volkswagen", "Golf", 5, False, "manual", 4, True, False, True, False),
-    Coche("3333AAA", "Sedán", "Tesla", "Model S", 5, True, "automático", 4, True, True, True, False),
-    Coche("4444BBB", "SUV", "Jeep", "Wrangler", 4, False, "manual", 3, False, True, False, True),
-    Coche("5555CCC", "Minivan", "Kia", "Carnival", 7, True, "automático", 5, True, True, True, True),
-    Coche("6666DDD", "Pickup", "Ford", "F-150", 5, False, "automático", 4, False, True, False, True),
-    Coche("7777EEE", "Coupé", "Porsche", "911", 2, True, "automático", 2, True, True, False, False),
-    Coche("8888FFF", "Hatchback", "Toyota", "Yaris", 5, False, "manual", 4, False, False, True, False),
-    Coche("9999GGG", "Sedán", "Hyundai", "Elantra", 5, False, "automático", 4, True, True, False, False),
-    Coche("1010HHH", "SUV", "Subaru", "Forester", 5, True, "manual", 5, True, True, True, True),
-    Coche("2020III", "Convertible", "BMW", "Z4", 2, True, "automático", 2, True, False, False, False)
+    Coche("1234ABC", "SUV", "Toyota", "RAV4", 5, True, "automático", 5, True, True, False, True, 1),
+    Coche("4200EXX", "SUV", "Hyundai", "Tucson", 5, True, "manual", 5, True, True, False, True, 1),
+    Coche("5678DEF", "Sedán", "Honda", "Civic", 5, False, "automático", 5, True, True, True, False, 1),
+    Coche("9101GHI", "Hatchback", "Ford", "Focus", 5, False, "manual", 3, False, True, False, False, 1),
+    Coche("1122JKL", "Coupé", "BMW", "Serie 4", 4, True, "automático", 3, True, True, False, False, 1),
+    Coche("3344MNO", "SUV", "Mercedes", "GLA", 5, True, "automático", 5, True, True, True, True, 1),
+    Coche("5566PQR", "Pickup", "Chevrolet", "Silverado", 5, False, "manual", 3, False, True, False, True, 1),
+    Coche("7788STU", "Sedán", "Audi", "A4", 5, True, "automático", 5, True, True, False, False, 2),
+    Coche("9900VWX", "Convertible", "Mazda", "MX-5", 2, True, "manual", 3, False, False, False, False, 2),
+    Coche("1111YYY", "SUV", "Nissan", "X-Trail", 7, True, "automático", 5, True, True, True, True, 2),
+    Coche("2222ZZZ", "Hatchback", "Volkswagen", "Golf", 5, False, "manual", 3, True, False, True, False, 2),
+    Coche("3333AAA", "Sedán", "Tesla", "Model S", 5, True, "automático", 3, True, True, True, False, 2),
+    Coche("4444BBB", "SUV", "Jeep", "Wrangler", 4, False, "manual", 3, False, True, False, True, 3),
+    Coche("5555CCC", "Minivan", "Kia", "Carnival", 7, True, "automático", 5, True, True, True, True, 3),
+    Coche("6666DDD", "Pickup", "Ford", "F-150", 5, False, "automático", 5, False, True, False, True,3),
+    Coche("7777EEE", "Coupé", "Porsche", "911", 2, True, "automático", 3, True, True, False, False, 3),
+    Coche("8888FFF", "Hatchback", "Toyota", "Yaris", 5, False, "manual", 5, False, False, True, False, 3),
+    Coche("9999GGG", "Sedán", "Hyundai", "Elantra", 5, False, "automático", 5, True, True, False, False, 3),
+    Coche("1010HHH", "SUV", "Subaru", "Forester", 5, True, "manual", 5, True, True, True, True, 3),
+    Coche("2020III", "Convertible", "BMW", "Z4", 2, True, "automático", 3, True, False, False, False,3 )
 ]
 
 for coche in coches:
     coche.guardar_en_db()
     print(coche.mostrar_info())
 
-coche_recuperado = Coche.obtener_desde_db("4200EXX")
-if coche_recuperado:
-    print(coche_recuperado.mostrar_info())
+#coche_recuperado = Coche.obtener_desde_db("4200EXX")
+#if coche_recuperado:
+#    print(coche_recuperado.mostrar_info())
