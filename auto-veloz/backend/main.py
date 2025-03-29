ecffcaf62f20c380dcf84c729fc8b2c26bbc6dbf
jose.marquez <jose.marquez@alumnos.upm.es>
2025-03-29 11:57:11 +0100

    Creado endpoint para front

--- auto-veloz/backend/main.py
+++ auto-veloz/backend/main.py
@@ -1,2 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from insertarOficinas import Oficina
 
+app = FastAPI()
+
+# Modelo para validar los datos de entrada
+class OficinaRequest(BaseModel):
+    nombre: str
+    direccion: str
+
+@app.post("/oficina/")
+def crear_oficina(oficina: OficinaRequest):
+    nueva_oficina = Oficina(oficina.nombre, oficina.direccion)
+    nueva_oficina.guardar_en_db()  # Llamamos a la función para guardar en la BD
+    return {"mensaje": "Oficina añadida correctamente", "nombre": oficina.nombre, "direccion": oficina.direccion}
 
