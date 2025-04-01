a66e7bff2c32d1c4a03ef8e2e6ab7da57461211b
jose.marquez <jose.marquez@alumnos.upm.es>
2025-04-01 17:06:09 +0200

    .

--- auto-veloz/backend/insertarOficinas.py
+++ auto-veloz/backend/insertarOficinas.py
@@ -1,6 +1,30 @@
 import psycopg2
 
 
+def obtener_oficinas():
+    conexion = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="DhYae8vU820ZZec7AW5S",
+        host="blablacardb.cjucoqgasy0s.eu-north-1.rds.amazonaws.com",
+        port="5432"
+    )
+    cursor = conexion.cursor()
+    cursor.execute(
+        """
+        SELECT nombre, direccion FROM oficina; 
+        """
+    )
+    oficina_data = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+
+    if oficina_data:
+        return [Oficina(*oficina) for oficina in oficina_data]
+    else:
+        return None
+
+
 class Oficina:
     def __init__(self, nombre, direccion):
         self.nombre = nombre
@@ -8,10 +32,10 @@ class Oficina:
         
 
     def mostrar_info(self):
-        return f"Nombre: {self.nombre}, Dirección: {self.direccion}, Matriculas: {self.matriculas}" \
+        return f"Nombre: {self.nombre}, Dirección: {self.direccion}" \
                
 
-    def guardar_en_db(self):
+    def guardar_oficina(self):
         conexion = psycopg2.connect(
             dbname="postgres",
             user="postgres",
@@ -39,33 +63,10 @@ class Oficina:
         cursor.close()
         conexion.close()
 
-# TODO: darle una vuelta esta funcion para que se imprima bien
-    @staticmethod
-    def obtener_desde_db(nombre):
-        conexion = psycopg2.connect(
-            dbname="postgres",
-            user="postgres",
-            password="DhYae8vU820ZZec7AW5S",
-            host="blablacardb.cjucoqgasy0s.eu-north-1.rds.amazonaws.com",
-            port="5432"
-        )
-        cursor = conexion.cursor()
-        cursor.execute(
-            """
-            SELECT * FROM oficina WHERE nombre = '
-            """
-            + nombre +
-            """
-            ';
-            """)
-        oficina_data = cursor.fetchone()
-        cursor.close()
-        conexion.close()
-        
-        if oficina_data:
-            return Oficina(*oficina_data)
-        else:
-            return None
+lista_oficinas = obtener_oficinas()
+for oficina in lista_oficinas:
+    print(oficina.mostrar_info())
+
 
 # Ejemplo de uso
 # oficina1 = Oficina("Office03", "Calle La calle, 10, Mostoles, Madrid")
--- auto-veloz/backend/main.py
+++ auto-veloz/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from insertarOficinas import Oficina
+from insertarOficinas import Oficina, obtener_oficinas
 
 app = FastAPI()
 
@@ -9,9 +9,16 @@ class OficinaRequest(BaseModel):
     nombre: str
     direccion: str
 
-@app.post("/oficina/")
+@app.post("/putOficina/")
 def crear_oficina(oficina: OficinaRequest):
     nueva_oficina = Oficina(oficina.nombre, oficina.direccion)
-    nueva_oficina.guardar_en_db()  # Llamamos a la función para guardar en la BD
+    nueva_oficina.guardar_oficina()  # Llamamos a la función para guardar en la BD
     return {"mensaje": "Oficina añadida correctamente", "nombre": oficina.nombre, "direccion": oficina.direccion}
 
+
+@app.get("/getOficinas/")
+def mostrar_oficinas():
+    lista_oficinas = obtener_oficinas()
+    for oficina in lista_oficinas:
+        print(oficina.mostrar_info())
+    return lista_oficinas
