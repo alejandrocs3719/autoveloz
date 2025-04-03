5847dcf7bd8e8f9ec5f418a5148385716d261e73
alejandrocs3719 <alejandro2917asm@gmail.com>
2025-04-03 10:00:21 +0200

    frontend bueno 3

--- auto-veloz/frontend/components/Marcas/SingleFeature.tsx
+++ auto-veloz/frontend/components/Marcas/SingleFeature.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Feature } from "@/types/Marca";
+import { Feature } from "@/types/marca";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
