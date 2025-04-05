3eafba664ea9946e79b098a0963610b182d3cb66
jose.marquez <jose.marquez@alumnos.upm.es>
2025-04-05 17:28:23 +0200

    Cambiado getOficinas para que devuelva tambien los ids

--- auto-veloz/backend/main.py
+++ auto-veloz/backend/main.py
@@ -25,16 +25,16 @@ class OficinaRequest(BaseModel):
 
 @app.post("/putOficina")
 def crear_oficina(oficina: OficinaRequest):
-    nueva_oficina = Oficina(oficina.nombre, oficina.direccion)
+    nueva_oficina = Oficina(0, oficina.nombre, oficina.direccion)
     nueva_oficina.guardar_oficina()  # Llamamos a la función para guardar en la BD
-    return {"mensaje": "Oficina añadida correctamente", "nombre": oficina.nombre, "direccion": oficina.direccion}
+    return {"mensaje": "Oficina añadida correctamente", "id":oficina.id, "nombre": oficina.nombre, "direccion": oficina.direccion}
 
 
 @app.get("/getOficinas")
 def mostrar_oficinas():
     lista_oficinas = obtener_oficinas()
-    for oficina in lista_oficinas:
-        print(oficina.mostrar_info())
+    # for oficina in lista_oficinas:
+    #     print(oficina.mostrar_info())
     return lista_oficinas
 
 from fastapi import Request
